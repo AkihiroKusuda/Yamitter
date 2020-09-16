f8e2af2afcf68aafca22e834b7372f1876b703e5
Akihiro Kusuda <akixda0001@gmail.com>
2020-09-17 04:43:09 +0900

    Add eslint-plugin-prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   env: {
     browser: true,
+    node: true
   },
   extends: [
     //'plugin:vue/essential', 
@@ -20,6 +21,7 @@ module.exports = {
   ],
   // required to lint *.vue files
   plugins: [
+    'prettier',
     'vue',
   ],
   // add your custom rules here
