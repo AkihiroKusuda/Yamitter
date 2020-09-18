21068a6fa86d6a74fd75ee725beada81fa635eab
MasaKusuda <masa.kusuda.1999@gmail.com>
2020-09-19 06:55:30 +0900

    natori

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" />
+    <img src="./assets/hq720.jpg" />
     <router-view />
   </div>
 </template>
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c3333;
   margin-top: 60px;
 }
 </style>
