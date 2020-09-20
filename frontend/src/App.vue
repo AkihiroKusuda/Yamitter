e7ecca04a9c1d2930918738203cae9429f039f3b
MasaKusuda <masa.kusuda.1999@gmail.com>
2020-09-20 15:46:16 +0900

    雑にいじいじ

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,76 @@
 <template>
   <div id="app">
-    <img src="./assets/hq720.jpg" />
+    <nav class="level">
+      <div class="level-item has-text-centered">
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                <a class="link is-info">Home</a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                <a class="link is-info">Menu</a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+      <p class="level-item has-text-centered">
+        <img src="https://bulma.io/images/bulma-type.png" alt style="height: 30px;" />
+      </p>
+      <div class="level-item has-text-centered">
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                <a class="link is-info">Reservations</a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                <a class="link is-info">Contact</a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </nav>
+    <div class="hero is-info">
+      <div class="hero-body">
+        <div class="container natori">
+          <h1 class="title">Natori Sana!</h1>
+          <h2 class="subtitle">Pink!ALL Pink!</h2>
+          <div>
+            <img src="./assets/hq720.jpg" />
+          </div>
+        </div>
+      </div>
+    </div>
     <router-view />
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Yamitter</strong>
+          by
+          <a href="https://github.com/MasaKusuda">Masakusu</a> and
+          <a href="https://github.com/AkihiroKusuda">AkihiroKusuda</a>
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
